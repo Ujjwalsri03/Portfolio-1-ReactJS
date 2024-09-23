d71e64ecf2c1f4cda5ce01a3de96065297dd2811
Ujjwalsri03 <ujjawalshriwastav9771@gmail.com>
2024-09-23 23:20:23 +0530

    adding pages

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import Navbar from './components/Navbar'
 import Home from './components/Home';
+import About from './components/About';
+import Navbar from './components/navbar';
+import Portfolio from './components/Portfolio';
+import Experience from './components/Experience';
+import Contact from './components/Contact'
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <About />
+      <Portfolio />
+      <Experience />
+      <Contact />
+
     </>
   )
 }
